metatoaster/mtj.flask.acl

commit 5a5e36e633657d65c8ade8fd49a16ba3c18ccaec

Clean up the initialization arguments
- Truely disable the default session handlers.  It really doesn't look
  like we need them.

mtj/flask/acl/principal.py
@@ -39,7 +39,7 @@ def acl_session_identity_saver(identity):
 def init_app(acl, app, mtjacl_sessions=True, *a, **kw):
 
     # Not using the default session.
-    principal = Principal(app, *a, **kw)
+    principal = Principal(app, use_sessions=False, *a, **kw)
 
     @identity_loaded.connect_via(app)
     def on_identity_loaded(sender, identity):

mtj/flask/acl/base.py
@@ -95,12 +95,12 @@ class BaseAcl(object):
     def updatePassword(self, login, password):
         return False
 
-    def __call__(self, app):
+    def __call__(self, app, *a, **kw):
         """
         Hook this ACL with the app
         """
 
-        self.init_app(app)
+        self.init_app(app, *a, **kw)
 
     def init_app(self, app, use_sessions=True, *a, **kw):
         """

commit 612ed2a32a35d183cfa49f95e9e215257e9f0995

Well, if we trash something..
- At least make sure the sleep deprived state doesn't result in a state
  where testing is forgotten.  Ensure the the required checks are also
  saved.  Fixes error introduced in 5a5e36e63365.

mtj/flask/acl/principal.py
@@ -34,6 +34,7 @@ def acl_session_identity_loader():
 def acl_session_identity_saver(identity):
     if isinstance(identity, AclIdentity):
         session['mtj.access_token'] = identity.access_token
+        session['identity.auth_type'] = identity.auth_type
         session.modified = True
 
 def handle_permission_denied(error):